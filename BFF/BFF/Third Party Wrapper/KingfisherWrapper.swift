0afce1faad1a9d9bc17c7677dd86a8ec6145ed07
yulinshin <shin.yulin0516@gmail.com>
2021-10-18 15:39:16 +0800

    set Kingfisher

---
+++ BFF/BFF/Third Party Wrapper/KingfisherWrapper.swift
@@ -0,0 +1,21 @@
+//
+//  KingfisherWrapper.swift
+//  BFF
+//
+//  Created by yulin on 2021/10/18.
+//
+
+import UIKit
+import Kingfisher
+
+extension UIImageView {
+
+    func loadImage(_ urlString: String?, placeHolder: UIImage? = nil) {
+
+        guard urlString != nil else { return }
+
+        let url = URL(string: urlString!)
+
+        self.kf.setImage(with: url, placeholder: placeHolder)
+    }
+}
