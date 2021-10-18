e9fcf44c7fe6559929d250964e7211c791e7533f
yulinshin <shin.yulin0516@gmail.com>
2021-10-18 23:26:54 +0800

    Set Models

---
+++ BFF/BFF/Model/Pet.swift
@@ -0,0 +1,40 @@
+//
+//  Pet.swift
+//  BFF
+//
+//  Created by yulin on 2021/10/18.
+//
+
+import Foundation
+import FirebaseFirestore
+
+struct Pet: Codable {
+
+    var petId: String
+    var name: String
+    var userId: String
+    var healthInfo: HealthInfo
+    var medicalRecords: [MedicalRecord]?
+    var petThumbnail: String
+    
+}
+
+struct HealthInfo: Codable {
+
+    var allergy: String
+    var birthday: String
+    var chipId: String
+    var gender: String
+    var note: String
+    var type: String
+    var weight: Double
+
+}
+
+struct MedicalRecord: Codable {
+
+    var images: [String]
+    var note: String
+    var time: Timestamp
+
+}
