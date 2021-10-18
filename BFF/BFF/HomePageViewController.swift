98265032a7a8bb5c49c7a8b93855eaf87e4dd443
yulinshin <shin.yulin0516@gmail.com>
2021-10-18 23:27:45 +0800

    Set FirebaseManger & show data on screen

--- BFF/BFF/HomePageViewController.swift
+++ BFF/BFF/HomePageViewController.swift
@@ -19,8 +19,51 @@ class HomePageViewController: UIViewController {
     }
     var sections = [Section.hero, Section.catalog, Section.petNotifycation, Section.pets]
 
+    var catalogIcon = ["diary", "supply", "heart", "goal"]
+    var catalogLable =  ["日記本", "用品", "健康", "成就"]
+
+    var user = User(userId: "", email: "", provider: "", providerId: "", userName: "", blockPets: [String](), petsIds: [String]()) {
+
+        didSet {
+            collectionView.reloadData()
+        }
+
+    }
+
+    var notifications = [Notification]() {
+
+        didSet {
+            collectionView.reloadData()
+        }
+
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        FirebaseManager.shared.fetchUser { result in
+            switch result {
+
+            case .success(let user):
+                self.user = user
+
+            case .failure(let error):
+                print("fetchData.failure\(error)")
+
+            }
+        }
+
+        FirebaseManager.shared.fetchNotifications { result in
+            switch result {
+
+            case .success(let notifications):
+                self.notifications = notifications
+
+            case .failure(let error):
+                print("fetchData.failure\(error)")
+
+            }
+        }
 
         collectionView.collectionViewLayout = createLayout()
         collectionView.delegate = self
@@ -28,10 +71,11 @@ class HomePageViewController: UIViewController {
 
     }
 
+    // MARK: - UICollectionViewLayout
 
     func createLayout() -> UICollectionViewLayout {
 
-        let layout = UICollectionViewCompositionalLayout { (sectionIndex, layoutEnvironment) -> NSCollectionLayoutSection? in
+        let layout = UICollectionViewCompositionalLayout { (sectionIndex, _) -> NSCollectionLayoutSection? in
 
             let section = self.sections[sectionIndex]
             switch section {
@@ -55,7 +99,6 @@ class HomePageViewController: UIViewController {
                 let item = NSCollectionLayoutItem(layoutSize: itemSize)
 
                 let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(120))
-
                 let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 4)
 
                 let layoutSection = NSCollectionLayoutSection(group: group)
@@ -71,7 +114,6 @@ class HomePageViewController: UIViewController {
                 let item = NSCollectionLayoutItem(layoutSize: itemSize)
 
                 let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(80))
-
                 let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
 
                 let layoutSection = NSCollectionLayoutSection(group: group)
@@ -83,7 +125,8 @@ class HomePageViewController: UIViewController {
 
                 let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
                 let item = NSCollectionLayoutItem(layoutSize: itemSize)
-                let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.6), heightDimension: .absolute(300))
+
+                let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.8), heightDimension: .absolute(300))
                 let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
 
                 let layoutSection = NSCollectionLayoutSection(group: group)
@@ -97,10 +140,12 @@ class HomePageViewController: UIViewController {
 
 }
 
+// MARK: - CollectionViewDelegate
 extension HomePageViewController: UICollectionViewDelegate {
 
 }
 
+// MARK: - UICollectionViewDataSource
 extension HomePageViewController: UICollectionViewDataSource {
 
     func numberOfSections(in collectionView: UICollectionView) -> Int {
@@ -108,17 +153,28 @@ extension HomePageViewController: UICollectionViewDataSource {
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+
         switch section {
         case 0:
+
             return 1
+
         case 1:
-            return 4 // Return catalogCount
+
+            return catalogIcon.count
+
         case 2:
-            return 1 // Return petNotificationCount
+
+            return notifications.count
+
         case 3:
-            return 1 // Return petsCount
+
+            return user.petsIds?.count ?? 0
+
         default:
+
             fatalError("SecionIndexOutOfRange")
+
         }
     }
 
@@ -130,11 +186,15 @@ extension HomePageViewController: UICollectionViewDataSource {
 
             guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "WellcomeCollectionViewCell", for: indexPath)as? WellcomeCollectionViewCell else { fatalError() }
 
+            cell.setup(userName: user.userName, petsCount: user.petsIds?.count ?? 0)
+
             return cell
 
         case 1:
 
             guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CatalogCollectionViewCell", for: indexPath)as? CatalogCollectionViewCell else { fatalError() }
+
+            cell.setup(title: catalogLable[indexPath.row], iconName: catalogIcon[indexPath.row])
 
             return cell
 
@@ -142,14 +202,44 @@ extension HomePageViewController: UICollectionViewDataSource {
 
             guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PetNotificationCollectionViewCell", for: indexPath)as? PetNotificationCollectionViewCell else { fatalError() }
 
+            guard let petId = notifications[indexPath.row].fromPets.first else { return cell }
+
+            FirebaseManager.shared.fetchPet(petId: petId) { result in
+                switch result {
+
+                case .success(let pet):
+
+                    cell.setup(petName: pet.name, content: self.notifications[indexPath.row].content, petImage: pet.petThumbnail)
+
+                case .failure(let error):
+
+                    print("fetchData.failure\(error)")
+
+                }
+            }
             return cell
 
         case 3:
 
             guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PetCollectionViewCell", for: indexPath)as? PetCollectionViewCell else { fatalError() }
 
-            return cell
+            guard let petId = user.petsIds?[indexPath.row] else { return cell }
 
+            FirebaseManager.shared.fetchPet(petId: petId) { result in
+                switch result {
+
+                case .success(let pet):
+
+                    cell.setup(petImage: pet.petThumbnail)
+
+                case .failure(let error):
+
+                    print("fetchData.failure\(error)")
+
+                }
+            }
+
+            return cell
         default:
             fatalError("SecionIndexOutOfRange")
 
---
+++ BFF/BFF/Network/FirebaseManager.swift
@@ -0,0 +1,82 @@
+//
+//  UserManager.swift
+//  BFF
+//
+//  Created by yulin on 2021/10/18.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestoreSwift
+
+class FirebaseManager {
+
+    static let shared = FirebaseManager()
+
+    lazy var dateBase = Firestore.firestore()
+
+    func fetchUser(completion: @escaping (Result<User, Error>) -> Void) {
+
+        dateBase.collection("Users").document("7QBGUfSDqPPjfJXRpQAI").getDocument { (document, error) in
+
+            if let document = document, document.exists {
+                do {
+                    if let user = try document.data(as: User.self, decoder: Firestore.Decoder()) {
+                        completion(.success(user))
+                    }
+                } catch {
+                    completion(.failure(error))
+                }
+              } else {
+                  fatalError()
+              }
+        }
+    }
+
+    func fetchNotifications(completion: @escaping (Result<[Notification], Error>) -> Void) {
+
+        dateBase.collection("Users").document("7QBGUfSDqPPjfJXRpQAI").collection("Notifications").getDocuments { (querySnapshot, error) in
+
+            if let error = error {
+
+                completion(.failure(error))
+            } else {
+
+                var notifications = [Notification]()
+
+                for document in querySnapshot!.documents {
+
+                    do {
+                        if let notification = try document.data(as: Notification.self, decoder: Firestore.Decoder()) {
+                            notifications.append(notification)
+                        }
+
+                    } catch {
+
+                        completion(.failure(error))
+                    }
+                }
+
+                completion(.success(notifications))
+            }
+        }
+    }
+
+    func fetchPet(petId: String, completion: @escaping (Result<Pet, Error>) -> Void) {
+
+        dateBase.collection("Pets").document(petId).getDocument { (document, error) in
+
+            if let document = document, document.exists {
+                do {
+                    if let pet = try document.data(as: Pet.self, decoder: Firestore.Decoder()) {
+                        completion(.success(pet))
+                    }
+                } catch {
+                    completion(.failure(error))
+                }
+              } else {
+                  fatalError()
+              }
+        }
+    }
+}
