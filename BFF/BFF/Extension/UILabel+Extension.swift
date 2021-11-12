a8d0e83deef706afb3ee5ab065c6eb84ddcf7037
yulinshin <shin.yulin0516@gmail.com>
2021-11-12 15:23:26 +0800

    set the privacy policy

--- BFF/BFF/Extension/UILabel+Extension.swift
+++ BFF/BFF/Extension/UILabel+Extension.swift
@@ -17,4 +17,40 @@ extension UILabel{
         guard numberOfLines > 0 else { return false }
         return countLabelLines() > numberOfLines
     }
+
+
+        func indexOfAttributedTextCharacterAtPoint(point: CGPoint) -> Int {
+            assert(self.attributedText != nil, "This method is developed for attributed string")
+            let textStorage = NSTextStorage(attributedString: self.attributedText!)
+            let layoutManager = NSLayoutManager()
+            textStorage.addLayoutManager(layoutManager)
+            let textContainer = NSTextContainer(size: self.frame.size)
+            textContainer.lineFragmentPadding = 0
+            textContainer.maximumNumberOfLines = self.numberOfLines
+            textContainer.lineBreakMode = self.lineBreakMode
+            layoutManager.addTextContainer(textContainer)
+
+            let index = layoutManager.characterIndex(for: point, in: textContainer, fractionOfDistanceBetweenInsertionPoints: nil)
+            return index
+        }
+}
+
+extension String {
+    static func format(strings: [String],
+                    boldFont: UIFont = UIFont.boldSystemFont(ofSize: 14),
+                    boldColor: UIColor = UIColor.blue,
+                    inString string: String,
+                    font: UIFont = UIFont.systemFont(ofSize: 14),
+                    color: UIColor = UIColor.black) -> NSAttributedString {
+        let attributedString =
+            NSMutableAttributedString(string: string,
+                                    attributes: [
+                                        NSAttributedString.Key.font: font,
+                                        NSAttributedString.Key.foregroundColor: color])
+        let boldFontAttribute = [NSAttributedString.Key.font: boldFont, NSAttributedString.Key.foregroundColor: boldColor]
+        for bold in strings {
+            attributedString.addAttributes(boldFontAttribute, range: (string as NSString).range(of: bold))
+        }
+        return attributedString
+    }
 }
